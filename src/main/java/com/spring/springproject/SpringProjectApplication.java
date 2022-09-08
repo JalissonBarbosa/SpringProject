8f4f7438830179116bfb94082c9f67d1027cb62c
JalissonBarbosa <jalissonbcampos@gmail.com>
2022-09-08 08:24:07 -0300

    Repository

---
+++ src/main/java/com/spring/springproject/SpringProjectApplication.java
@@ -0,0 +1,23 @@
+package com.spring.springproject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		categoryRepository.save();
+	}
+}
